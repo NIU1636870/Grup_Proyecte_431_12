e5895759405efdeedf8c8c35720caa9bf14ff860
NIU1636870 <1636870@uab.cat>
2024-06-04 23:03:33 +0200

    segonaEntrega

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined  (_WIN64)
 
 #include <iostream>
-//Definicio necesaria per poder incloure la llibreria i que trobi el main
+
 #define SDL_MAIN_HANDLED
 #include <windows.h>
 #include "Tetris.h"
-//Llibreria grafica
+
 #include "../Graphic Lib/libreria.h"
 #include "../Graphic Lib/NFont/NFont.h"
 #include <conio.h>      /* getch */ 
 
 #elif __APPLE__
-//Llibreria grafica
+
 #include "../Graphic Lib/libreria.h"
 #include "../Graphic Lib/NFont/NFont.h"
 #pragma clang diagnostic push
